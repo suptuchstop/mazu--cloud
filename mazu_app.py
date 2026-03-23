23569ff835647d4d1979cee9c8a6f59d9e9759ec
suptuchstop <suptuchstop@gmail.com>
2026-03-23 15:20:10 +0800

    Add files via upload

--- mazu_app.py
+++ mazu_app.py
@@ -249,7 +249,7 @@ if info_df is not None:
         colC.metric("去程", info["去程"])
         colD.metric("回程", info["回程"])
         colE.metric("登轎出發", format_time(info["登轎出發"]))
-        colF.metric("抵達北港", str(info["抵達北港"]))
+        colF.metric("抵達北港", pd.to_datetime(info["抵達北港"]).strftime("%m/%d"))
         colG.metric("刈火", format_time(info["刈火"]))
         colH.metric("回鑾", format_time(info["回鑾"]))
         st.metric("報名人數", f"{int(info['報名人數']):,}" if pd.notna(info["報名人數"]) else "-")
